bcc8ec16363cf6894d0e23efa920ae8dcca91403
Jingjing Deng <jjdengjj@users.noreply.github.com>
2024-12-07 23:18:03 +0000

    Change button to switch

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+function invert(){
+  // document.getElementById("profile").style.filter="invert(100%)";
+  document.getElementById("teamlogo").style.filter="invert(100%)";
+  document.getElementById("githublogo").style.filter="invert(100%)";
+  document.getElementById("email").style.filter="invert(100%)";
+  };
+
+  function noninvert(){
+    // document.getElementById("profile").style.filter="invert(0%)";
+    document.getElementById("teamlogo").style.filter="invert(0%)";
+    document.getElementById("githublogo").style.filter="invert(0%)";
+    document.getElementById("email").style.filter="invert(0%)";
+    };
+
+
 let themeToggler = document.getElementById('theme-toggler');
 
 themeToggler.onclick = () => {
@@ -6,7 +21,14 @@ themeToggler.onclick = () => {
 
   if (themeToggler.classList.contains('fa-sun')) {
     document.documentElement.classList.add('dark-mode');
+    invert();
   } else {
     document.documentElement.classList.remove('dark-mode');
+    noninvert();
   }
+};
+
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  // dark mode
+  document.getElementById('theme-toggler').click();
 };
